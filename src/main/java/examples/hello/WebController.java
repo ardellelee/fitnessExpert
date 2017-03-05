0260ff8174607a41cd5e354086093d20bc345a46
ardellelee <ardellelee@163.com>
2017-03-05 16:25:54 +0800

    update source codes to solve the sync problem

--- src/main/java/examples/hello/WebController.java
+++ src/main/java/examples/hello/WebController.java
@@ -33,11 +33,12 @@ public class WebController extends WebMvcConfigurerAdapter {
         String[] answers = {
                 "(assert(weight "+args.getWeight()+"))",
                 "(assert(height "+args.getHeight()+"))",
-                "(assert(age "+args.getAge()+"))",
-                // "(assert(gender "+args.getIsMale()+"))",
-                // "(assert(age "+args.getIsVegetarian()+"))"
+                "(assert(agee "+args.getAge()+"))",
+                "(assert(control yes))",
+                "(assert(tips 1))"
 
         };
+
 
 
         /*========================*/
@@ -45,16 +46,274 @@ public class WebController extends WebMvcConfigurerAdapter {
         /*========================*/
 
         clips = new Environment();
-        clips.loadFromResource("/testBmiBmr.clp");
+        clips.loadFromResource("/mine_ext-withcf2.clp");
         clips.reset();
 
         // input
         for (String factString : answers) {
             clips.eval(factString);
         }
+
+        // assert gender
+        // don't use "(assert(age "+args.getIsMale()+"))" in answers, as facts in clips is M/F instead of 0/1
+        if (args.getIsMale() == 1){
+            clips.eval("(assert (gender M))");
+        } else {
+            clips.eval("(assert (gender F))");
+        }
+
+        //assert age
+        //in clips logic, age is divided into 4 levels, so no use to send the exact age.
+        int age = args.getAge();
+        if(age<18){
+            clips.eval("(assert (age 18L "+age+"))");
+        }
+        else if(age<30){
+            clips.eval("(assert (age 30L "+age+"))");
+        }
+        else if(age<50){
+            clips.eval("(assert (age 50L "+age+"))");
+        }
+        else
+        {
+            clips.eval("(assert (age 80L "+age+"))");
+        }
+
+
+        //assert DietCertain
+            clips.eval("(assert (current_fact (fact follow-diet) (cf "+args.getIsDietCertain()+")))");
+        //assert ExerciseCertain
+            clips.eval("(assert (current_fact (fact follow-exercise) (cf "+args.getIsExerciseCertain()+")))");
+        //assert MealCertain
+            clips.eval("(assert (current_fact (fact follow-meal) (cf "+args.getIsMealCertain()+")))");
+        //assert MedicalCertain
+            clips.eval("(assert (current_fact (fact medical-hist) (cf "+args.getIsMedicalCertain()+")))");
+
+        //assert vegetarian
+        if (args.getIsVegetarian() == 1){
+            clips.eval("(assert (foodtype vegetarian))");
+        } else {
+            clips.eval("assert (foodtype nonvegetarian))");
+        }
+
+        //assert allergy
+        if(args.getIsNone() == Boolean.TRUE){
+            clips.eval("(assert (allergy none))");
+        }
+        else if(args.getIsCelery() == Boolean.TRUE){
+            clips.eval("(assert (allergy celery))");
+        }
+        else if(args.getIsCerealsAndGluten()==Boolean.TRUE){
+            clips.eval("(assert (allergy cereals))");
+        }
+        else if(args.getIsLupin()==Boolean.TRUE){
+            clips.eval("(assert (allergy lipin))");
+        }
+        else if(args.getIsMilk()==Boolean.TRUE){
+            clips.eval("(assert (allergy milk))");
+        }
+        else if(args.getIsMustard()==Boolean.TRUE){
+            clips.eval("(assert (allergy mustard))");
+        }
+        else if(args.getIsSesame()==Boolean.TRUE){
+            clips.eval("(assert (allergy sesame))");
+        }
+        else if(args.getIsSoyaAndSulphides()==Boolean.TRUE){
+            clips.eval("(assert (allergy soya))");
+        }
+
         clips.run();
-        String evalStr ="(find-all-facts ((?f UI-state)) TRUE)";
-        FactAddressValue fv = (FactAddressValue) ((MultifieldValue) clips.eval(evalStr)).get(0);
+
+        String breakfastcalorie ="(find-all-facts ((?f breakfastcalorie-fin)) TRUE)";
+        //get the value of breakfastcalorie'mealname
+        FactAddressValue fvbreakfastcalorie = (FactAddressValue) ((MultifieldValue) clips.eval(breakfastcalorie)).get(0);
+
+        String factConfidence = "(find-all-facts ((?f current_goal)) TRUE)";
+        FactAddressValue fvConfidence = (FactAddressValue) ((MultifieldValue) clips.eval(factConfidence)).get(0);
+        String strBreakfast ="";
+        try {
+            strBreakfast = fvbreakfastcalorie.getFactSlot("mealname").toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String lunchCalorie ="(find-all-facts ((?f lunchcalorie-fin)) TRUE)";
+        //get the value of lunchcalorie'mealname
+        FactAddressValue fvlunchCalorie = (FactAddressValue) ((MultifieldValue) clips.eval(lunchCalorie)).get(0);
+        String strLunch ="";
+        try {
+            strLunch = fvlunchCalorie.getFactSlot("mealname").toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String evalStrbmi ="(find-all-facts ((?f UI-state-bmi)) TRUE)";
+        //get BMI and BMR
+        FactAddressValue fvbmi = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrbmi)).get(0);
+
+
+        String evalStrbmr ="(find-all-facts ((?f UI-state-bmr)) TRUE)";
+        FactAddressValue fvbmr = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrbmr)).get(0);
+        //get breakfast 1
+        String evalStrTest = "(find-all-facts ((?f "+strBreakfast+")) (eq ?f:type B))";
+        FactAddressValue fvbreakfast1Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTest)).get(0);
+        //get breakfast 2
+
+        FactAddressValue fvbreakfast2Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTest)).get(1);
+        FactAddressValue fvbreakfast3Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTest)).get(2);
+        FactAddressValue fvbreakfast4Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTest)).get(3);
+        FactAddressValue fvbreakfast5Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTest)).get(4);
+        FactAddressValue fvbreakfast6Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTest)).get(5);
+        String evalStrTestLunch = "(find-all-facts((?f "+strLunch+"))(eq ?f:type L))";
+        FactAddressValue fvLunch1Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTestLunch)).get(0);
+        //get lunch
+
+        String evalStrTestDinner = "(find-all-facts((?f "+strLunch+"))(eq ?f:type D))";
+
+        FactAddressValue fvLunch2Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTestLunch)).get(1);
+        FactAddressValue fvLunch3Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTestLunch)).get(2);
+
+        FactAddressValue fvLunch4Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTestDinner)).get(0);
+        FactAddressValue fvLunch5Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTestDinner)).get(1);
+        FactAddressValue fvLunch6Name = (FactAddressValue) ((MultifieldValue) clips.eval(evalStrTestDinner)).get(2);
+       // exercise part
+        String output ="(find-all-facts ((?f UI-state-output)) TRUE)";
+        String output1 ="(find-all-facts ((?f UI-state-output1)) TRUE)";
+        FactAddressValue fvoutput = (FactAddressValue) ((MultifieldValue) clips.eval(output)).get(0);
+        FactAddressValue fvoutput1 = (FactAddressValue) ((MultifieldValue) clips.eval(output1)).get(0);
+        String exerciseOutput = "";
+        String exerciseOutput1 = "";
+        try {
+            exerciseOutput = fvoutput.getFactSlot("output").toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            exerciseOutput1 = fvoutput1.getFactSlot("output1").toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("xxxxxx");
+        System.out.println(exerciseOutput);
+        System.out.println("xxxxxx");
+        System.out.println(exerciseOutput1);
+        System.out.println("xxxxxx");
+        String Outputvalule = "test";
+        String Outputvalule1 = "test1";
+        int exerciseOutputint =Integer.valueOf(exerciseOutput).intValue();
+
+
+        if(exerciseOutputint==1)
+        {
+            Outputvalule = "Avoid staying up late";
+            Outputvalule1 = "Don't eat your meals while watching TV and or a movie";
+        }
+        else if(exerciseOutputint==2)
+        {
+            Outputvalule = "Avoid skipping meals. Especially Breakfast";
+            Outputvalule1 = "Increase your intake of carbohydrates and protein heavy food";
+        }
+        else if(exerciseOutputint==3)
+        {
+            Outputvalule = "Avoid staying up late as 8 hours of sleep is absolutely necessary";
+            Outputvalule1 = "Do not eat anything 4 hours before bedtime";
+        }
+        else if(exerciseOutputint==4)
+        {
+            Outputvalule = "Unhealthy foods will degrade your metabolism. Fruits are your friend";
+            Outputvalule1 = "Do not forget to eat your meals";
+        }
+        else if(exerciseOutputint==5)
+        {
+            Outputvalule = "Try to cut down on the instant noodles and take-out food";
+            Outputvalule1 = "Focus on cardio if your belly is mostly beer";
+        }
+        else if(exerciseOutputint==6)
+        {
+            Outputvalule = "Replace junk food with granola bars and/or other protein heavy, healthier options";
+            Outputvalule1 = "Avoid skipping meals. Especially Breakfast!";
+        }
+        else if(exerciseOutputint==7)
+        {
+            Outputvalule = "Avoid mid-day snacks and restrict your diet to three wholesome meals";
+            Outputvalule1 = "Make fitness a habit, not a chore!";
+        }
+        else if(exerciseOutputint==8)
+        {
+            Outputvalule = "Add protein supplements and healthy calories such as nuts";
+            Outputvalule1 = "Try to plan a diet with several mini meals instead of 3 heavy meals";
+        }
+        else if(exerciseOutputint==9)
+        {
+            Outputvalule = "Focus on adding muscle mass by undertaking more muscle-heavy exercises";
+            Outputvalule1 = "Frame a comfortable diet plan and follow it diligently!";
+        }
+        else if(exerciseOutputint==10)
+        {
+            Outputvalule = "Avoid alcohol and other unhealthy recreational activities";
+            Outputvalule1 = "Do not forget the importance of core strength! Bulk up your core muscles";
+        }
+        else if(exerciseOutputint==11)
+        {
+            Outputvalule = "Try to maintain a regular workout pattern";
+            Outputvalule1 = "Don't eat your meals while watching TV and/or a movie";
+        }
+        else if(exerciseOutputint==12)
+        {
+            Outputvalule = "Avoid heavy dinners";
+            Outputvalule1 = "Do not increase the rate of your workout without prior consultation";
+        }
+        System.out.println(Outputvalule);
+
+        int exerciseOutputint1 =Integer.valueOf(exerciseOutput1).intValue();
+        String Outputvalule2 = "test2";
+        String Outputvalule3 = "test3";
+        if(exerciseOutputint1==1)
+        {
+            Outputvalule2 = "Ideal Weekly Exercise Plan: 3 days of Cardio Exercise (Maybe Sports and/or Cycling)";
+            Outputvalule3 = "Ideal Weekly Exercise Plan: 2 days of Weight Workout";
+        }
+        else if(exerciseOutputint1==2)
+        {
+            Outputvalule2 = "Ideal Weekly Exercise Plan: 1 days of Cardio Exercise (Maybe Sports and/or Cycling)";
+            Outputvalule3 = "Ideal Weekly Exercise Plan: 2 days of Weight Workout";
+        }
+        else if(exerciseOutputint1==3)
+        {
+            Outputvalule2 = "Ideal Weekly Exercise Plan: 2 days of Cardio Exercise (Maybe Swimming and/or Cycling";
+            Outputvalule3 = "Ideal Weekly Exercise Plan: 2 days of Weight Workout";
+        }
+        else if(exerciseOutputint1==4)
+        {
+            Outputvalule2 = "Ideal Weekly Exercise Plan: 1 days of Cardio Exercise (Maybe Running and/or Cycling)";
+            Outputvalule3 = "Ideal Weekly Exercise Plan: 2 days of Weight Workout";
+        }
+        else if(exerciseOutputint1==5)
+        {
+            Outputvalule2 = "Ideal Weekly Exercise Plan: 2 days of Cardio Exercise (Maybe Swimming and/or Running)";
+            Outputvalule3 = "Ideal Weekly Exercise Plan: 2 days of Weight Workout";
+        }
+        else if(exerciseOutputint1==6)
+        {
+            Outputvalule2 = "Ideal Weekly Exercise Plan: 1 days of Cardio Exercise (Maybe Swimming and/or Cycling)";
+            Outputvalule3 = "Ideal Weekly Exercise Plan: 2 days of Weight Workout";
+        }
+        else if(exerciseOutputint1==7)
+        {
+            Outputvalule2 = "Ideal Weekly Exercise Plan: 3 days of Cardio Exercise (Maybe Swimming and/or Running)";
+            Outputvalule3 = "Ideal Weekly Exercise Plan: 2 days of Weight Workout";
+        }
+        else if(exerciseOutputint1==8)
+        {
+            Outputvalule2 = "Ideal Weekly Exercise Plan: 2 days of Cardio Exercise (Maybe Sports and/or Cycling)";
+            Outputvalule3 = "Ideal Weekly Exercise Plan: 2 days of Weight Workout";
+        }
+        else if(exerciseOutputint1==9)
+        {
+            Outputvalule2 = "Ideal Weekly Exercise Plan: 2 days of Cardio Exercise (Maybe Cycling and/or Running)";
+            Outputvalule3 = "Ideal Weekly Exercise Plan: 2 days of Weight Workout";
+        }
+
 
         ret.put("success", true);
 
@@ -62,7 +321,12 @@ public class WebController extends WebMvcConfigurerAdapter {
         ret.put("response", resp);
         resp.put("age", args.getAge());
         resp.put("height", args.getHeight());
+        resp.put("respect1",Outputvalule);
+        resp.put("respect2",Outputvalule1);
+        resp.put("respect3",Outputvalule2);
+        resp.put("respect4",Outputvalule3);
         resp.put("weight", args.getWeight());
+
         if (args.getIsMale() == 1){
             resp.put("gender", "Male");
         } else {
@@ -76,17 +340,171 @@ public class WebController extends WebMvcConfigurerAdapter {
 
         //output
         try{
-            resp.put("bmi", ((FloatValue) fv.getFactSlot("bmi")).floatValue()) ;
+            resp.put("bmi", ((FloatValue) fvbmi.getFactSlot("bmi")).floatValue()) ;
         } catch (Exception e){
             System.out.println(e.getStackTrace());
         }
 
         try{
-            resp.put("bmr", ((FloatValue) fv.getFactSlot("bmr")).floatValue()) ;
+            resp.put("bmr", ((FloatValue) fvbmr.getFactSlot("bmr")).floatValue()) ;
         } catch (Exception e){
             System.out.println(e.getStackTrace());
         }
 
+        try{
+            resp.put("breakfast1Name", (fvbreakfast1Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast2Name", (fvbreakfast2Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast3Name", (fvbreakfast3Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast4Name", (fvbreakfast4Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast5Name", (fvbreakfast5Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast6Name", (fvbreakfast6Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+
+        try{
+            resp.put("breakfast1Qty", (fvbreakfast1Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast2Qty", (fvbreakfast2Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast3Qty", (fvbreakfast3Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast4Qty", (fvbreakfast4Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast5Qty", (fvbreakfast5Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("breakfast6Qty", (fvbreakfast6Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+
+        try{
+            resp.put("lunch1Name", (fvLunch1Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch2Name", (fvLunch2Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch3Name", (fvLunch3Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch4Name", (fvLunch4Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch5Name", (fvLunch5Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch6Name", (fvLunch6Name.getFactSlot("name").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+
+        try{
+            resp.put("lunch1Qty", (fvLunch1Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch2Qty", (fvLunch2Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch3Qty", (fvLunch3Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch4Qty", (fvLunch4Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch5Qty", (fvLunch5Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        try{
+            resp.put("lunch6Qty", (fvLunch6Name.getFactSlot("qty").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+
+        try{
+            resp.put("confidence", (fvConfidence.getFactSlot("cf").toString())) ;
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+        clips.clear();
         return ret;
     }
 }
--- src/main/resources/static/js/view/form.js
+++ src/main/resources/static/js/view/form.js
@@ -24,6 +24,10 @@ var form;
             var isSoyaAndSulphides = $("#submitForm").find("input[name=isSoyaAndSulphides]").is(":checked");
             var isMale = $("#submitForm input[name='gender']:checked").val();
             var isVegetarian = $("#submitForm input[name='foodType']:checked").val();
+            var isDietCertain = $("#dietCertain").val();
+            var isExerciseCertain = $("#exerciseCertain").val();
+            var isMealCertain = $("#mealCertain").val();
+            var isMedicalCertain=$("#medicalCertain").val();
             var args = {
                 age: age,
                 height: height,
@@ -37,21 +41,67 @@ var form;
                 isMilk: isMilk,
                 isMustard: isMustard,
                 isSesame: isSesame,
-                isSoyaAndSulphides: isSoyaAndSulphides
+                isSoyaAndSulphides: isSoyaAndSulphides,
+                isDietCertain: isDietCertain,
+                isExerciseCertain:isExerciseCertain,
+                isMealCertain:isMealCertain,
+                isMedicalCertain:isMedicalCertain
             };
             Nicecom.Net.Ajax.post("/", args).trigger(e.target).json(function (u) {
                 if (u.success) {
                     //页面显示
-                    $("#Detail").find("#detailHeight").text(u.response.height);
-                    $("#Detail").find("#detailWeight").text(u.response.weight);
-                    $("#Detail").find("#detailAge").text(u.response.age);
-                    $("#Detail").find("#detailGender").text(u.response.gender);
-                    $("#Detail").find("#detailFoodType").text(u.response.foodType);
-                    $("#Detail").find("#detailAllergy").text(u.response.allergy);
-                    $("#Detail").find("#detailBMI").text(u.response.bmi);
-                    $("#Detail").find("#detailBMR").text(u.response.bmr);
-                    $("#Detail").find("#detailToB").text(u.response.toB);
-                    $("#Detail").find("#detailRecommend").text(u.response.recommend);
+                    $("#detailHeight").text(u.response.height);
+                    $("#detailWeight").text(u.response.weight);
+                    $("#detailAge").text(u.response.age);
+                    $("#detailGender").text(u.response.gender);
+                    $("#detailFoodType").text(u.response.foodType);
+                    $("#detailAllergy").text(u.response.allergy);
+                    $("#detailBMI").text(u.response.bmi);
+                    $("#detailBMR").text(u.response.bmr);
+                    $("#detailToB").text(u.response.toB);
+                    $("#detailPlan").text(u.response.plan);
+                    $("#detailRecommend").text(u.response.recommend);
+                    $("#fooditemb1").text(u.response.breakfast1Name);
+                    $("#fooditemb2").text(u.response.breakfast2Name);
+                    $("#fooditemb3").text(u.response.breakfast3Name);
+                    $("#fooditemb4").text(u.response.breakfast4Name);
+                    $("#fooditemb5").text(u.response.breakfast5Name);
+                    $("#fooditemb6").text(u.response.breakfast6Name);
+                    $("#quantityb1").text(u.response.breakfast1Qty);
+                    $("#quantityb2").text(u.response.breakfast2Qty);
+                    $("#quantityb3").text(u.response.breakfast3Qty);
+                    $("#quantityb4").text(u.response.breakfast4Qty);
+                    $("#quantityb5").text(u.response.breakfast5Qty);
+                    $("#quantityb6").text(u.response.breakfast6Qty);
+                    $("#fooditeml1").text(u.response.lunch1Name);
+                    $("#fooditeml2").text(u.response.lunch2Name);
+                    $("#fooditeml3").text(u.response.lunch3Name);
+                    $("#fooditeml4").text(u.response.lunch4Name);
+                    $("#fooditeml5").text(u.response.lunch5Name);
+                    $("#fooditeml6").text(u.response.lunch6Name);
+                    $("#quantityl1").text(u.response.lunch1Qty);
+                    $("#quantityl2").text(u.response.lunch2Qty);
+                    $("#quantityl3").text(u.response.lunch3Qty);
+                    $("#quantityl4").text(u.response.lunch4Qty);
+                    $("#quantityl5").text(u.response.lunch5Qty);
+                    $("#quantityl6").text(u.response.lunch6Qty);
+                    $("#fooditemd1").text(u.response.lunch1Name);
+                    $("#fooditemd2").text(u.response.lunch2Name);
+                    $("#fooditemd3").text(u.response.lunch3Name);
+                    $("#fooditemd4").text(u.response.lunch4Name);
+                    $("#fooditemd5").text(u.response.lunch5Name);
+                    $("#fooditemd6").text(u.response.lunch6Name);
+                    $("#quantityd1").text(u.response.lunch1Qty);
+                    $("#quantityd2").text(u.response.lunch2Qty);
+                    $("#quantityd3").text(u.response.lunch3Qty);
+                    $("#quantityd4").text(u.response.lunch4Qty);
+                    $("#quantityd5").text(u.response.lunch5Qty);
+                    $("#quantityd6").text(u.response.lunch6Qty);
+                    $("#detailConfidence").text(u.response.confidence);
+                    $("#exercise1").text(u.response.respect1);
+                    $("#exercise2").text(u.response.respect2);
+                    $("#exercise3").text(u.response.respect3);
+                    $("#exercise4").text(u.response.respect4);
                     $('#mySubmit').modal({ keyboard: false }).show();
                 }
                 else {
